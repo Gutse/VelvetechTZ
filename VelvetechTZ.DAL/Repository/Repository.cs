d2d564ff98b81d9e6ccaf7ff40c10b220ec5c3d3
daniil_k <key.dach@gmail.com>
2020-09-30 17:03:49 +0300

    add empty repository implementation

---
+++ VelvetechTZ.DAL/Repository/Repository.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using VelvetechTZ.Domain.BaseModel;
+
+namespace VelvetechTZ.DAL.Repository
+{
+    public class Repository<T> : IRepository<T> where T : IBaseModel
+    {
+        public Task<T> GetById(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteById(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<long> Insert(T model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Update(T model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
