74e7bbdcb952a3e9541bc045d1c789bac2b35414
daniil_k <key.dach@gmail.com>
2020-09-30 16:38:51 +0300

    add swagger

--- VelvetechTZ.API/Startup.cs
+++ VelvetechTZ.API/Startup.cs
@@ -1,8 +1,12 @@
+using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace VelvetechTZAPI
 {
@@ -18,12 +22,29 @@ namespace VelvetechTZAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Velvetech TZ api",
+                    Version = "v1",
+
+                    Description = "A simple example ASP.NET Core Web API",
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
+                app.UseSwagger();
+
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Velvetech TZ API V1"));
+
                 app.UseDeveloperExceptionPage();
             }
 
