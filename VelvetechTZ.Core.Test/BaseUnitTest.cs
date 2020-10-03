76ae4f91957a6ca52a1e274ca058fdbc2b70a84d
daniil_k <key.dach@gmail.com>
2020-10-03 15:46:06 +0300

    add tests

---
+++ VelvetechTZ.Core.Test/BaseUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Autofac;
+using AutoMapper;
+using VelvetechTZ.Core.Core;
+
+namespace VelvetechTZ.Core.Test
+{
+    public class BaseUnitTest
+    {
+        protected static readonly IMapper mapper;
+        protected static readonly IContainer container;
+        static BaseUnitTest()
+        {
+            var builder = new ContainerBuilder();
+            builder.ConfigureCoreContainer(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
+            container = builder.Build();
+            mapper = container.Resolve<IMapper>();
+        }
+    }
+}
---
+++ VelvetechTZ.Core.Test/GroupServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using VelvetechTZ.Contract.Domain.Group;
+using VelvetechTZ.Core.Group;
+using VelvetechTZ.DAL.Models.Group;
+using VelvetechTZ.DAL.Repository;
+using Xunit;
+
+namespace VelvetechTZ.Core.Test
+{
+    public class GroupServiceTest: BaseUnitTest
+    {
+
+        private GroupService sut;
+        private readonly Mock<IRepository<GroupModel>> groupRepository = new Mock<IRepository<GroupModel>>();
+
+        public GroupServiceTest()
+        {
+            sut = new GroupService(groupRepository.Object, mapper);
+        }
+
+        [Fact]
+        public async Task Should_pass_params_Correctly()
+        {
+            var contract = new GroupContract
+            {
+                Name = "Test Group"
+            };
+
+            groupRepository.Setup(r => r.Insert(It.IsAny<GroupModel>())).ReturnsAsync((GroupModel model) =>
+            {
+                model.Name.Should().Be(contract.Name);
+                return 1;
+            });
+
+            var result = await sut.Create(contract);
+            groupRepository.VerifyAll();
+        }
+    }
+}
